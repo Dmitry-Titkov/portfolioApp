30aa9f0a60aeba21e87633325e04e1b9605ca780
Dmitry-Titkov <dmitry.titkov94@gmail.com>
2020-06-30 09:29:05 +0200

    added selectors and reducers

--- src/store/AppState/selector.js
+++ src/store/AppState/selector.js
@@ -1,5 +1,5 @@
 export const selectAppLoading = (state) => state.appState.loading;
 export const selectMessage = (state) => state.appState.message;
-export const selectArtworks = (reduxState) => {
+export const selectAuctions = (reduxState) => {
   return reduxState.art;
 };
